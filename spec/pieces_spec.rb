6c6cd2b9827c1d112cb4f6bcab296123ef683398
Blondy <blondymartinezm@gmail.com>
2024-03-19 00:37:01 +0100

    movement tests for each piece

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -37,6 +37,130 @@ describe "Pieces" do
           
             expect(piece_to_move.position).to eq(initial_position)
             expect(board.slot_empty?(target_position)).to be false
-          end
+        end
+
+        it "allows pawn to capture diagonally" do 
+            player1 = Player.new('black')
+            player2 = Player.new('white')
+            board = Board.new(player1.pieces.concat(player2.pieces))
+        
+            pawn = player1.pieces.flatten.find { |piece| piece.is_a?(Pawn) }
+            board.update_piece_pos(pawn, [3, 0])
+
+            pawn_to_capture = player2.pieces.flatten.find { |piece| piece.is_a?(Pawn) }
+            target_position = [2, 1]
+
+            if pawn.valid_move?(target_position, board)
+                board.update_piece_pos(pawn, target_position)
+            end
+        
+            expect(pawn.position).to eq(target_position)
+            expect(board.slot_empty?([3, 0])).to be true
+        end
+
+        it "does not allow pawn to perform initial double move if it has moved already" do 
+            player1 = Player.new('black')
+            player2 = Player.new('white')
+            board = Board.new(player1.pieces.concat(player2.pieces))
+        
+            pawn = player1.pieces.flatten.find { |piece| piece.is_a?(Pawn) }
+            target_position = [3, 0]
+            board.update_piece_pos(pawn, target_position)
+            pawn.first_move
+
+            initial_position = pawn.position
+            target_position = [5, 0]
+
+            if pawn.valid_move?(target_position, board)
+                board.update_piece_pos(pawn, target_position)
+            end
+
+            expect(pawn.is_first_move).to be false
+            expect(pawn.position).to eq(initial_position)
+        end
+
+        it "allows king to move one square in any direction" do
+            player1 = Player.new('black')
+            player2 = Player.new('white')
+            board = Board.new(player1.pieces.concat(player2.pieces))
+        
+            king = player1.pieces.flatten.find { |piece| piece.is_a?(King) }
+            king.position = [3, 3] # places the piece where no other pieces are aroud
+            initial_position = king.position
+            target_position = [initial_position[0] + 1, initial_position[1] + 1]
+
+            if king.valid_move?(target_position, board)
+                board.update_piece_pos(king, target_position)
+            end
+
+            expect(king.position).to eq(target_position)
+        end
+
+        it "allows queen to move diagonally" do
+            player1 = Player.new('black')
+            player2 = Player.new('white')
+            board = Board.new(player1.pieces.concat(player2.pieces))
+        
+            queen = player1.pieces.flatten.find { |piece| piece.is_a?(Queen) }
+            queen.position = [3, 3] # places the piece where no other pieces are aroud
+            initial_position = queen.position
+            target_position = [initial_position[0] + 2, initial_position[1] + 2]
+
+            if queen.valid_move?(target_position, board)
+                board.update_piece_pos(queen, target_position)
+            end
+
+            expect(queen.position).to eq(target_position)
+        end
+
+        it "allows rook to move horizontally" do
+            player1 = Player.new('black')
+            player2 = Player.new('white')
+            board = Board.new(player1.pieces.concat(player2.pieces))
+        
+            rook = player1.pieces.flatten.find { |piece| piece.is_a?(Rook) }
+            rook.position = [3, 3] # places the piece where no other pieces are aroud
+            initial_position = rook.position
+            target_position = [initial_position[0], initial_position[1] + 2]
+
+            if rook.valid_move?(target_position, board)
+                board.update_piece_pos(rook, target_position)
+            end
+
+            expect(rook.position).to eq(target_position)
+        end
+
+        # it "allows knight to move in an L-shape" do
+        #     player1 = Player.new('black')
+        #     player2 = Player.new('white')
+        #     board = Board.new(player1.pieces.concat(player2.pieces))
+        
+        #     knight = player1.pieces.flatten.find { |piece| piece.is_a?(Knight) }
+        #     initial_position = knight.position
+        #     target_position = [initial_position[0] - 2, initial_position[1] - 1]
+
+        #     if knight.valid_move?(target_position, board)
+        #         board.update_piece_pos(knight, target_position)
+        #     end
+
+        #     expect(knight.position).to eq(target_position)
+        # end
+
+        it "allows bishop to move diagonally" do
+            player1 = Player.new('black')
+            player2 = Player.new('white')
+            board = Board.new(player1.pieces.concat(player2.pieces))
+        
+            bishop = player1.pieces.flatten.find { |piece| piece.is_a?(Bishop) }
+            bishop.position = [4, 4] # places the piece where no other pieces are aroud
+            initial_position = bishop.position
+            target_position = [initial_position[0] - 2, initial_position[1] - 2]
+
+            if bishop.valid_move?(target_position, board)
+                board.update_piece_pos(bishop, target_position)
+            end
+
+            expect(bishop.position).to eq(target_position)
+        end
     end
 end
